9848002fd0d4d720bd5467d8a97ea9fc5790691c
marty1313170 <martin.han1@education.nsw.gov.au>
2026-03-05 15:10:06 +1100

    Refactored code for correct placement, may require extra attention

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ root.geometry("600x400")
 root.grid_columnconfigure(0, weight=1)
 frm = tk.Frame(root)
 frm.grid()
-tk.Label(frm, text="Welcome to the EarthQuake DIRECTORY!!", font=("impact", 24) ).grid(column=0, row=0)
-tk.Button(frm, text="Search").grid(column=0, row=2)
+b = tk.Label(frm, text="Welcome to the EarthQuake DIRECTORY!!", font=("impact", 24) ).grid(column=0, row=0)
+a = tk.Button(frm, text="Search")
+    
+a.place(x=240, y=70)
 
 
 
 entry = tk.Entry(root)
-entry.grid(column=2, row=4)
+entry.place(x=240, y=50)
+
+
 
 
 root.mainloop()
+
+
+
+
